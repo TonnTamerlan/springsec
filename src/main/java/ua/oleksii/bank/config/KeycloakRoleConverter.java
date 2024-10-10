79f2144e8ae45ee3cc17d0625ff36a7e1890f172
Oleksii Kopylov <tonn.tamerlan@gmail.com>
2024-10-10 23:52:44 +0100

    add auth server

--- src/main/java/ua/oleksii/bank/config/KeycloakRoleConverter.java
+++ src/main/java/ua/oleksii/bank/config/KeycloakRoleConverter.java
@@ -1,6 +1,6 @@
 package ua.oleksii.bank.config;
 
-import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
@@ -9,19 +9,16 @@ import org.springframework.security.oauth2.jwt.Jwt;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Configuration
 public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
     @Override
     public Collection<GrantedAuthority> convert(Jwt source) {
-        Map<String, Object> realmAccess = (Map<String, Object>) source.getClaims().get("realm_access");
-        if (MapUtils.isEmpty(realmAccess)) {
+        var roles = (ArrayList<String>) source.getClaims().get("roles");
+        if (CollectionUtils.isEmpty(roles)) {
             return new ArrayList<>();
         }
-        var roles = (List<String>) realmAccess.get("roles");
         return roles.stream()
                 .map(roleName -> "ROLE_" + roleName)
                 .map(SimpleGrantedAuthority::new)
